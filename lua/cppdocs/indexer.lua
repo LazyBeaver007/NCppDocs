1191133658cd879363c2ab2fdd0fad20ca5af91b
LazyBeaver007 <adiprime007@gmail.com>
2026-04-13 12:30:32 +0530

    Indexer added-working

---
+++ lua/cppdocs/indexer.lua
@@ -0,0 +1,51 @@
+local M = {}
+local function scan_dir(dir, files)
+    files = files or {}
+
+    local handle = vim.loop.fs_scandir(dir)
+    if not handle then 
+        return files 
+    end 
+
+
+    while true do
+        local name, type = vim.loop.fs_scandir_next(handle)
+        if not name then break end
+
+        local full_path = dir .. "/" .. name
+        if type == "directory" then
+            scan_dir(full_path, files)
+        elseif type == "file" and name:match("%.html$") then
+            table.insert(files, full_path)
+        end
+    end
+
+    return files 
+    
+end 
+
+local function extract_key(filepath)
+    local name = filepath:match("([^/]+)%.html$")
+    return name
+    
+end
+
+
+function M.build_index(root_path)
+    local index = {}
+    local files = scan_dir(root_path)
+
+    for _,file in ipairs(files) do 
+        local key  = extract_key(file)
+
+        if key then
+            if not index[key] then 
+                index[key] = file
+            end
+        end
+    end
+    return index
+    
+end
+
+return M
---
+++ lua/init.lua
@@ -0,0 +1,2 @@
+:lua local idx = require("cppdocs.indexer").build_index("D:/html_book_20190607/reference/en"); 
+print(vim.inspect(idx))
